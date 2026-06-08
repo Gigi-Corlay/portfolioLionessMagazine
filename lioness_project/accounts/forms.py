af7a4c1e83e5ae07f8b6305c5105460888bf0a06
Gigi-Corlay <11489@holbertonstudents.com>
2026-06-08 11:30:43 +0200

    Add shared header, footer and auth styles

--- lioness_project/accounts/forms.py
+++ lioness_project/accounts/forms.py
@@ -4,14 +4,73 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+
+    last_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            "placeholder": "Your Name",
+            "class": "form-control"
+        })
+    )
+
+    first_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            "placeholder": "Your First Name",
+            "class": "form-control"
+        })
+    )
+
+    country = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            "placeholder": "Your Country",
+            "class": "form-control"
+        })
+    )
+
+    occupation = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            "placeholder": "Your Job",
+            "class": "form-control"
+        })
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            "placeholder": "E-mail",
+            "class": "form-control"
+        })
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Password",
+            "class": "form-control"
+        })
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Confirm Password",
+            "class": "form-control"
+        })
+    )
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = [
+            "last_name",
+            "first_name",
+            "email",
+            "password1",
+            "password2",
+        ]
 
 
 class LoginForm(AuthenticationForm):
+
     username = forms.CharField(
         widget=forms.EmailInput(attrs={
             "placeholder": "E-mail",
--- lioness_project/accounts/views.py
+++ lioness_project/accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 
 from .forms import RegisterForm
+from .models import Profile
 
 
 def login_view(request):
@@ -22,11 +23,33 @@ def register_view(request):
 
     if request.method == "POST":
         if form.is_valid():
-            user = form.save()
+
+            user = form.save(commit=False)
+
+            user.first_name = form.cleaned_data["first_name"]
+            user.last_name = form.cleaned_data["last_name"]
+            user.email = form.cleaned_data["email"]
+
+            # si tu veux te connecter avec l'email
+            user.username = form.cleaned_data["email"]
+
+            user.save()
+
+            Profile.objects.create(
+                user=user,
+                country=form.cleaned_data["country"],
+                occupation=form.cleaned_data["occupation"]
+            )
+
             login(request, user)
+
             return redirect("home")
 
-    return render(request, "accounts/register.html", {"form": form})
+    return render(
+        request,
+        "accounts/register.html",
+        {"form": form}
+    )
 
 
 def logout_view(request):
