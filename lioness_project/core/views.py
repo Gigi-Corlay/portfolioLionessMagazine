af54897abdcd9d47cef5866cf9563c1c71a41be1
Gigi-Corlay <11489@holbertonstudents.com>
2026-06-25 14:58:45 +0200

    fix(legal-pages): fix legal pages rendering and add favicon

--- lioness_project/core/views.py
+++ lioness_project/core/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+
+def legal_notice(request):
+    return render(request, "legals_notices/legal_notice.html")
+
+
+def privacy_policy(request):
+    return render(request, "legals_notices/privacy_policy.html")
+
+
+def terms_of_use(request):
+    return render(request, "legals_notices/terms_of_use.html")
+
+
+def cookie_policy(request):
+    return render(request, "legals_notices/cookie_policy.html")
