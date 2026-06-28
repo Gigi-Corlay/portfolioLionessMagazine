47e660796b8c1ecb80762b6ce51e8fa19d51f917
Gigi-Corlay <11489@holbertonstudents.com>
2026-06-28 16:57:15 +0200

    Fix build.sh paths for Render

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+
 # exit on error
 set -o errexit
+
+# On force le script à entrer dans le bon dossier s'il n'y est pas déjà
+cd $STATIC_ROOT/../lioness_project 2>/dev/null || cd lioness_project 2>/dev/null || true
 
 pip install -r requirements.txt
 
