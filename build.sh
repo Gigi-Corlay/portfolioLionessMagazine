7da111eebca635bc62e17d29932bc887591d21bd
Gigi-Corlay <11489@holbertonstudents.com>
2026-06-28 17:22:29 +0200

    Save gunicorn path during build

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-
 # exit on error
 set -o errexit
 
-# On force le script à entrer dans le bon dossier s'il n'y est pas déjà
+# On force l'entrée dans le dossier du projet
 cd $STATIC_ROOT/../lioness_project 2>/dev/null || cd lioness_project 2>/dev/null || true
 
+# Installation des dépendances
 pip install -r requirements.txt
 
+# On sauvegarde le chemin absolu de gunicorn pour la Start Command
+echo "$(which gunicorn)" > gunicorn_path.txt
+
+# Commandes Django
 python manage.py collectstatic --no-input
 python manage.py migrate
