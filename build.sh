34ef4ff67728d9faa76c1446e27bf753bab1bbb6
Gigi-Corlay <11489@holbertonstudents.com>
2026-06-28 17:40:44 +0200

    Standardize build with environment activation

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 # exit on error
 set -o errexit
 
-# On force l'entrée dans le dossier du projet
-cd $STATIC_ROOT/../lioness_project 2>/dev/null || cd lioness_project 2>/dev/null || true
+# On remonte d'un niveau pour activer l'environnement virtuel global de Render
+if [ -f "../.venv/bin/activate" ]; then
+    source ../.venv/bin/activate
+fi
 
-# Installation des dépendances
+# On s'assure d'installer les paquets au bon endroit
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# On sauvegarde le chemin absolu de gunicorn pour la Start Command
-echo "$(which gunicorn)" > gunicorn_path.txt
-
-# Commandes Django
+# Commandes Django standard
 python manage.py collectstatic --no-input
 python manage.py migrate
