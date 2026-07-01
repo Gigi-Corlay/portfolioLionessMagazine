6f559797847794621a101a455a61abe376791308
Gigi-Corlay <11489@holbertonstudents.com>
2026-07-01 13:00:28 +0200

    Fix media/image handling for production on Render (ImageKit + templates)

--- lioness_project/config/urls.py
+++ lioness_project/config/urls.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 
-
 urlpatterns = [
     # Admin
     path("admin/", admin.site.urls),
@@ -93,7 +92,4 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
