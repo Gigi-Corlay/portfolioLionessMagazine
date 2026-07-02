233846f6a27bac3c38439121a99b373a917181a0
Gigi-Corlay <11489@holbertonstudents.com>
2026-07-02 16:13:10 +0200

    fix: remove unused slug field from Article model

--- lioness_project/blog_magazine/models.py
+++ lioness_project/blog_magazine/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from ckeditor.fields import RichTextField
-from django.utils.text import slugify
 
 
 class Article(models.Model):
-
     CATEGORY_CHOICES = [
         ('ACTUS', 'ACTUS'),
         ('MOOD', 'MOOD'),
@@ -20,45 +18,16 @@ class Article(models.Model):
     ]
 
     title = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True, blank=True, max_length=255)
-
     author = models.CharField(max_length=150, blank=True)
-
     chapo = RichTextField(blank=True, null=True)
     texte = RichTextField()
-
-    image = models.ImageField(
-        upload_to="articles/",
-        blank=True,
-        null=True
-    )
-
-    category = models.CharField(
-        max_length=50,
-        choices=CATEGORY_CHOICES,
-        default='ACTUS'
-    )
-
+    image = models.ImageField(upload_to="articles/", blank=True, null=True)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='ACTUS')
     published = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ["-created_at"]
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            base_slug = slugify(self.title)
-            slug = base_slug
-            counter = 1
-
-            # évite doublons de slug (IMPORTANT en production)
-            while Article.objects.filter(slug=slug).exists():
-                slug = f"{base_slug}-{counter}"
-                counter += 1
-
-            self.slug = slug
-
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
