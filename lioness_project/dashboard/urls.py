022b51858f6e06b1b849b859b359286a7761b79e
Gigi-Corlay <11489@holbertonstudents.com>
2026-07-12 15:23:18 +0200

    Move landing page to root for GitHub Pages

--- lioness_project/dashboard/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import dashboard_view, dashboard_search
-
-urlpatterns = [
-    path("", dashboard_view, name="dashboard"),
-    path("search/", dashboard_search, name="dashboard_search"),
-]
